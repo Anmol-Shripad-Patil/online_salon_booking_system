3d1e5a34c14dff826fbf258775d34f7312feec92
Anmol Shripad Patil <anmolpatil.5241@gmail.com>
2023-09-02 11:53:30 +0530

    commited

--- ASP.NET/esalon/Models/SalonRevenue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace esalon.Models
-{
-    public class SalonRevenue
-    {
-        public String SalonName { get; set; }
-        public int SalonId { get; set; }
-        public decimal TotalRevenue { get; set; }
-    }
-}
